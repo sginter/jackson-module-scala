41cee7d43138f394a864f008a8ed91f085882a75
Slawomir Ginter <sginter@atlassian.com>
2014-03-22 13:39:43 +0100

    Synchronize on Universe not particular mirror to avoid initiation races

--- src/main/scala/com/fasterxml/jackson/module/scala/util/ScalaBeansUtil.scala
+++ src/main/scala/com/fasterxml/jackson/module/scala/util/ScalaBeansUtil.scala
@@ -1,7 +1,7 @@
 package com.fasterxml.jackson.module.scala.util
-import scala.reflect.runtime.universe._
 import scala.reflect.api.JavaUniverse
 import org.codehaus.jackson.annotate.JsonProperty
+import java.util.concurrent.TimeUnit
 
 trait Property { val name: String; val primaryName: String }
 
@@ -10,8 +10,11 @@ case class FieldGetter(name: String, primaryName: String) extends Property
 case class FieldSetter(name: String, primaryName: String) extends Property
 
 object ScalaBeansUtil {
+  /* hopefully object creation is synchronized enough to do the synchronization */
+  private val universe = scala.reflect.runtime.universe
+  import universe._
 
-  def getJsonPropertyValue (annotations: Seq[JavaUniverse#Annotation])  = {
+  private def getJsonPropertyValue (annotations: Seq[JavaUniverse#Annotation])  = {
     val JsonPropertyValueName = newTermName("value")
 
     val jsonPropertyValues = for {
@@ -23,33 +26,32 @@ object ScalaBeansUtil {
     jsonPropertyValues.headOption
   }
 
-  def getParamsAnnotations(method: MethodSymbolApi) = method.paramss.flatten.map(p => (p.name.decoded, p.annotations))
+  private def getParamsAnnotations(method: MethodSymbolApi) = method.paramss.flatten.map(p => (p.name.decoded, p.annotations))
 
-  def propertiesOf(cls: Class[_]): Seq[Property] = {
+  def propertiesOf(cls: Class[_]): Seq[Property] = universe.synchronized {
     val mirror = runtimeMirror(cls.getClassLoader)
-    mirror.synchronized {
-      val clsType = mirror.classSymbol(cls).toType
+    val clsType = mirror.classSymbol(cls).toType
 
-      val constructor = clsType.declaration(nme.CONSTRUCTOR)
-      val ctorParamDecls = for {
-        ((paramName, annotations), index) <- getParamsAnnotations(constructor.asMethod).zipWithIndex
-        annotationValue = getJsonPropertyValue(annotations)
-      } yield (ConstructorParameter(paramName, annotationValue.getOrElse(paramName), index), (paramName, annotationValue))
+    val constructor = clsType.declaration(nme.CONSTRUCTOR)
+    val ctorParamDecls = for {
+      ((paramName, annotations), index) <- getParamsAnnotations(constructor.asMethod).zipWithIndex
+      annotationValue = getJsonPropertyValue(annotations)
+    } yield (ConstructorParameter(paramName, annotationValue.getOrElse(paramName), index), (paramName, annotationValue))
 
-      val (constructorParameters, annotations) = ctorParamDecls.unzip
-      val ctorParamAnnotations = Map() ++ (for {
-        (paramName, Some(annotationValue)) <- annotations
-      } yield (paramName, annotationValue))
+    val (constructorParameters, annotations) = ctorParamDecls.unzip
+    val ctorParamAnnotations = Map() ++ (for {
+      (paramName, Some(annotationValue)) <- annotations
+    } yield (paramName, annotationValue))
 
-      constructorParameters ++ clsType.declarations.collect {
-        case m: MethodSymbol if m.isGetter || m.isSetter =>
-          val field = m.accessed.name.decoded.trim //workaround for SI-8137/SI-5736
-          val fromAnnotation = getJsonPropertyValue(m.annotations)
-            .orElse(getJsonPropertyValue(m.accessed.annotations))
-            .orElse(ctorParamAnnotations.get(field))
-          val name = fromAnnotation.getOrElse(field)
-          if (m.isSetter) FieldSetter(field, name) else FieldGetter(field, name)
-      }
+    constructorParameters ++ clsType.declarations.collect {
+      case m: MethodSymbol if m.isGetter || m.isSetter =>
+        //workaround for SI-8137/SI-5736
+        val field = m.accessed.name.decoded.trim
+        val fromAnnotation = getJsonPropertyValue(m.annotations)
+          .orElse(getJsonPropertyValue(m.accessed.annotations))
+          .orElse(ctorParamAnnotations.get(field))
+        val name = fromAnnotation.getOrElse(field)
+        if (m.isSetter) FieldSetter(field, name) else FieldGetter(field, name)
     }
   }
 }
